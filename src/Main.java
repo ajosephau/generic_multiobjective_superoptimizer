1b8a061ecc463c493bc7726e3ec30fc96e1413a2
Anthony Joseph <ajoseph811@gmail.com>
2014-12-18 11:42:38 +1100

    adding supporting notes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
-import org.antlr.v4.runtime.*;
-import org.antlr.v4.runtime.tree.*;
-import org.dgso.antlrv4parser.*;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.dgso.antlrv4parser.ANTLRv4Lexer;
+import org.dgso.antlrv4parser.ANTLRv4Parser;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
@@ -10,16 +12,23 @@ public class Main {
     public static void main(String[] args) throws Exception {
         // create a CharStream that reads from standard input
         String inputFile = null;
-        if ( args.length>0 ) inputFile = args[0];
+        if (args.length > 0){
+            inputFile = args[0];
+        }
         InputStream is = System.in;
-        if ( inputFile!=null ) is = new FileInputStream(inputFile);
+        if (inputFile != null) {
+            is = new FileInputStream(inputFile);
+        }
         ANTLRInputStream input = new ANTLRInputStream(is);
         ANTLRv4Lexer lexer = new ANTLRv4Lexer(input); // create a buffer of tokens pulled from the lexer
         CommonTokenStream tokens = new CommonTokenStream(lexer); // create a parser that feeds off the tokens buffer
         ANTLRv4Parser parser = new ANTLRv4Parser(tokens);
+
+        //start parsing the "grammarSpec" rule in the ANTLRv4 rule
         ParseTree tree = parser.grammarSpec();
         ANTLRv4Visitor av = new ANTLRv4Visitor();
         av.visit(tree);
+
         System.out.println(tree.toStringTree(parser)); // print LISP-style tree
     }
 }
