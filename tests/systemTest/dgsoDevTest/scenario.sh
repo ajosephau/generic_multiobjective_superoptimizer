fef255b10980eb615c6935fa2068ea9125b3a5ae
Anthony Joseph <ajoseph811@gmail.com>
2015-01-07 17:28:11 +1100

    adding updated test scripts

--- tests/systemTest/dgsoDevTest/scenario.sh
+++ tests/systemTest/dgsoDevTest/scenario.sh
@@ -1,1 +1,2 @@
 #!/bin/bash
+echo `tests/systemTest/dgsoDevTest/scenario1.sh $2` `tests/systemTest/dgsoDevTest/scenario2.sh $1 $2`
--- tests/systemTest/dgsoDevTest/test.sh
+++ tests/systemTest/dgsoDevTest/test.sh
@@ -2,7 +2,7 @@
 
 # Deterministic test to see if the program outputted matches expected values:
 
-if grep -q "variableA" "$1"; then
+if grep -q "variableA" "$2"; then
    echo "true test found variableA"
    exit 0
 else
