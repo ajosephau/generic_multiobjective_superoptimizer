05a5ca991670f0c02188b4320baa4df1ab6e7765
Anthony Joseph <anthonyjoeyjo@gmail.com>
2015-02-18 22:16:18 +1100

    Removing redundant waiting times in scripts

--- tests/lejos_experiment/scenarios/runLeJOSScenarios.bat
+++ tests/lejos_experiment/scenarios/runLeJOSScenarios.bat
@@ -3,7 +3,6 @@ CALL nxjc %2 > nul
 CD %1
 CALL nxjlink --output LeJOSPerfScen.nxj LeJOSPerfScen > nul
 CALL nxjupload --run --usb LeJOSPerfScen.nxj > nul
-CALL :sleep 5
 CALL nxjconsole > nxjConsoleOutput.txt
 FOR /f "delims=" %%a IN ('..\..\getFileSize.bat LeJOSPerfScen.nxj') DO @SET fileSize=%%a
 FOR /f "delims=" %%a IN ('..\..\getComputationTime.bat') DO @SET computationTime=%%a
@@ -11,9 +10,3 @@ ECHO %fileSize% %computationTime%
 DEL LeJOSPerfScen.class
 DEL LeJOSPerfScen.nxj
 DEL nxjConsoleOutput.txt
-CALL :sleep 10
-
-REM from http://superuser.com/questions/48231
-:sleep
-ping 127.0.0.1 -n 2 -w 1000 > NUL
-ping 127.0.0.1 -n %1 -w 1000 > NUL
--- tests/lejos_experiment/tests/runLeJOSFnTest.bat
+++ tests/lejos_experiment/tests/runLeJOSFnTest.bat
@@ -3,15 +3,8 @@ CALL nxjc %2 > nul
 CD %1
 CALL nxjlink --output LeJOSFnTest.nxj LeJOSFnTest > nul
 CALL nxjupload --run --usb LeJOSFnTest.nxj > nul
-CALL :sleep 5
 CALL nxjconsole > output.txt
 FINDSTR "Number of failures" output.txt
 DEL LeJOSFnTest.nxj
 DEL LeJOSFnTest.class
 DEL output.txt
-CALL :sleep 10
-
-REM from http://superuser.com/questions/48231
-:sleep
-ping 127.0.0.1 -n 2 -w 1000 > NUL
-ping 127.0.0.1 -n %1 -w 1000 > NUL
